a1f69fd76685867722334625dc707e3984f53ede
chengq220 <chengqiu220@gmail.com>
2026-03-05 16:14:31 -0500

    update to setnece transformer

--- train.sh
+++ train.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 python main.py \
-    --dataset endovis2017 \
+    --dataset endovis2018 \
     --sam2_version 'med' \
-    --name_exp endovis2017_med \
+    --name_exp endovis2018_med \
     --num_frames 8 \
     --max_skip 1 \
     --epochs 5 \
-    --batch_size 2  \
+    --batch_size 1  \
     --HSA \
     --lr 1e-5
