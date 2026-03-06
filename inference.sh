b49d7b8727dae8af6a17b2adf3309aee9fc31ae3
chengq220 <chengqiu220@gmail.com>
2026-03-06 12:34:12 -0500

    update

--- inference.sh
+++ inference.sh
@@ -10,12 +10,25 @@
 #     --threshold 0.5 \
 #     --HSA 
 
-for seq in seq1 seq2 seq3 seq4 seq5 seq6 seq7 seq8 seq9 seq10; do
+# for seq in seq1 seq2 seq3 seq4 seq5 seq6 seq7 seq8 seq9 seq10; do
+#     python inference.py \
+#         --sam2_version 'med' \
+#         --input_path "data/endovis2017/valid/JPEGImages/$seq" \
+#         --mask_input "data/endovis2017/valid/VOS/$seq" \
+#         --resume output/endovis2017_med/checkpoint0004.pth \
+#         --text_prompts 'all' \
+#         --num_frames 8 \
+#         --threshold 0.5 \
+#         --HSA
+# done
+
+
+for seq in seq2 seq5 seq9 seq15; do
     python inference.py \
         --sam2_version 'med' \
         --input_path "data/endovis2017/valid/JPEGImages/$seq" \
         --mask_input "data/endovis2017/valid/VOS/$seq" \
-        --resume output/endovis2017_med/checkpoint0004.pth \
+        --resume output/endovis2018_med/checkpoint0002.pth \
         --text_prompts 'all' \
         --num_frames 8 \
         --threshold 0.5 \
